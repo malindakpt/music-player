5a329d0f5810c9f33044bb390d90f8accc9119e6
MalindaK <kumarasinghe@axinom.com>
2020-05-16 02:43:37 +0530

    Code refactored

--- src/ContainerComponents/Container/Container.tsx
+++ src/ContainerComponents/Container/Container.tsx
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Container.module.scss";
 import { PlayList } from "../PlayList/PlayList";
 import { PlayerController } from "../PlayerController/PlayerController";
 import { Track } from "../../Model/Track";
 import { TrackInfo } from "../../PresentationComponents/TrackInfo/TrackInfo";
+import axios from "axios";
 
 export const Container: React.FC = () => {
-  const trackList: Track[] = [
-    {
-      name: "Inga sunga nalwana",
-      url:
-        "https://firebasestorage.googleapis.com/v0/b/musicplayer-8e865.appspot.com/o/Inga_Sunga_Nalawana_Buddhika_Ushan_Sarigama_lk%20-%20Copy.mp3?alt=media&token=04b9d6ab-ca4f-48dc-9b16-c2f63f008e29",
-      metaData: {
-        album: "album name",
-        thumbnailUrl:
-          "https://a10.gaanacdn.com/images/song/5/24091705/crop_480x480_1537454512.jpg",
-        artist: "artist name",
-        title: "title",
-      },
-    },
-  ];
+  const [trackList, setTrackList] = useState<Track[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/playlist.json")
+      .then((result) => setTrackList(result.data));
+  }, []);
 
   const [selectedTrack, setSelectedTrack] = useState<Track>();
 
