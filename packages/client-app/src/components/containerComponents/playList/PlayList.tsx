5cd7c44b650d021ee601acaaa6b16aa2af206ba6
MalindaK <kumarasinghe@axinom.com>
2020-05-16 22:41:34 +0530

    Show current playing song in playlist

--- packages/client-app/src/components/containerComponents/playList/PlayList.tsx
+++ packages/client-app/src/components/containerComponents/playList/PlayList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Track } from "../../../model/Track";
 import { PlayListEntry } from "../../presentationComponents/playListEntry/PlayListEntry";
 import classes from "./PlayList.module.scss";
@@ -10,10 +10,22 @@ export const PlayList: React.FC<PlayListProps> = ({
   trackList,
   onTrackSelect,
 }) => {
+  const [currentPlayId, setCurrentPlayId] = useState(-1);
+
+  const handleOnSelectSong = (track: Track) => {
+    setCurrentPlayId(track.id);
+    onTrackSelect(track);
+  };
+
   return (
     <div className={classes.container}>
       {trackList.map((track) => (
-        <PlayListEntry key={track.id} track={track} onSelect={onTrackSelect} />
+        <PlayListEntry
+          key={track.id}
+          track={track}
+          onSelect={handleOnSelectSong}
+          isPlaying={currentPlayId === track.id}
+        />
       ))}
     </div>
   );
--- packages/client-app/src/components/presentationComponents/playListEntry/PlayListEntry.tsx
+++ packages/client-app/src/components/presentationComponents/playListEntry/PlayListEntry.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Track } from "../../../model/Track";
 import classes from "./PlayListEntry.module.scss";
+import clsx from "clsx";
 
 export interface PlayListEntryProps {
   track: Track;
+  isPlaying: boolean;
   onSelect: (track: Track) => void;
 }
 
 export const PlayListEntry: React.FC<PlayListEntryProps> = ({
   track,
+  isPlaying,
   onSelect,
 }) => {
   return (
-    <div className={classes.container} onClick={() => onSelect(track)}>
+    <div
+      className={clsx(classes.container, { [classes.isPlaying]: isPlaying })}
+      onClick={() => onSelect(track)}
+    >
       {track.metaData.title}
     </div>
   );
