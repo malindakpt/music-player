9d3c580576fd68e1a39b5a8cdb8e98b2fd11fab4
MalindaK <kumarasinghe@axinom.com>
2020-05-16 10:22:48 +0530

    Added default thumb Image

--- src/PresentationComponents/TrackInfo/TrackInfo.tsx
+++ src/PresentationComponents/TrackInfo/TrackInfo.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./TrackInfo.module.scss";
 import { TrackMetaData } from "../../Model/TrackMetaData";
+import thumb from "../../Images/thumb.png";
 
 export interface TrackInfoProps {
   trackMetaData?: TrackMetaData;
 }
 export const TrackInfo: React.FC<TrackInfoProps> = ({
   trackMetaData = {
-    title: "Select a track",
+    title: "Select a song",
     artist: "Music Player",
     album: "version 1.0.0",
   },
@@ -20,9 +21,12 @@ export const TrackInfo: React.FC<TrackInfoProps> = ({
         <div>{trackMetaData.album}</div>
       </div>
       <div className={classes.thumbnail}>
-        {trackMetaData.thumbnailUrl && (
-          <img alt={trackMetaData.title} src={trackMetaData.thumbnailUrl} />
-        )}
+        {
+          <img
+            alt={trackMetaData.title}
+            src={trackMetaData.thumbnailUrl || thumb}
+          />
+        }
       </div>
     </div>
   );
